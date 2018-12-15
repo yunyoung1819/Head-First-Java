bdfd08623479c310ebd14c4408749cf3e28e146e
yunyoung1819 <yunyoung1819@hanmail.net>
2018-12-16 05:04:09 +0900

    chapter 7 inheritance 코드 추가

---
+++ src/chapter7/Doctor.java
@@ -0,0 +1,15 @@
+package chapter7;
+
+/**
+ * 
+ * 상속 
+ *
+ */
+public class Doctor {
+	
+	boolean worksAtHospital;
+	
+	void treatPatient() {
+		// 진료를 합니다.
+	}
+}
---
+++ src/chapter7/FamilyDoctor.java
@@ -0,0 +1,10 @@
+package chapter7;
+
+public class FamilyDoctor extends Doctor {
+
+	boolean makesHouseCalls;
+	
+	void giveAdvice() {
+		// 집에서 필요한 조언을 합니다.
+	}
+}
