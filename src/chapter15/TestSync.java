a1448d3438d490ebd56b1ef87a03277ae76240c5
yunyoung1819 <yunyoung1819@hanmail.net>
2019-03-15 23:57:55 +0900

    chapter 15. practice code add

---
+++ src/chapter15/TestSync.java
@@ -0,0 +1,22 @@
+package chapter15;
+
+/**
+ * 무시무시한 갱신 내용 손실 문제 
+ *
+ */
+class TestSync implements Runnable {
+	private int balance;
+
+	@Override
+	public void run() {
+		for(int i = 0; i < 50; i++) {
+			increment();
+			System.out.println("balance is " + balance);
+		}
+	}
+	
+	public void increment() {
+		int i = balance;
+		balance = i + 1;
+	}
+}
